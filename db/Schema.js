dc7479a209ca31d2b43b2cf66e687491fbc74d5e
Sebastian LoPiano <76269922+slopiano@users.noreply.github.com>
2022-01-25 12:49:34 -0500

    Updated Schema

--- db/Schema.js
+++ db/Schema.js
@@ -1,31 +1,43 @@
 const mongoose = require('mongoose');
 const { required } = require('nodemon/lib/config');
 
-const MySchema = new mongoose.Schema({
-  title: {
+const Books = new mongoose.Schema({
+  type: {
+    type: String,
+    //required: [true, "must include type"],
+    trim: true,
+    maxlength: [40, 'cannot be longer than 40 characters']
+  },
+  income: {
+    type: Boolean,
+    //required: [true, "must include author"],
+  },
+  price: {
+    type: Number,
+    //required: [true, "must include price"],
+  },
+  description: {
+    type: String,
+    default: 'No description'
+  },
+})
+
+const User = new mongoose.Schema({
+  Name: {
     type: String,
     required: [true, "must include title"],
     trim: true,
     maxlength: [40, 'cannot be longer than 40 characters']
   },
-  author: {
+  FavoriteAnimal: {
     type: String,
     required: [true, "must include author"],
     trim: true,
     maxlength: [30, 'cannot be longer than 30 characters']
   },
-  genre: {
-    type: String,
-    default: "Fiction"
-  },
-  price: {
-    type: Number,
-    required: [true, "must include price"],
-  },
-  url: {
-    type: String,
-    default: 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png'
-  },
 })
 
-module.exports = mongoose.model('Schema', MySchema)
+const BookModel = mongoose.model('books', Books)
+const UserModel = mongoose.model('user', User)
+
+module.exports = {BookModel, UserModel}
